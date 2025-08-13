f44cffbeac3b3aa5f2b88b3195145c8151a67bc4
Harshada Ghanwat <harshadaofficial665@gmail.com>
2025-08-14 00:01:47 +0530

    Add ProfileTab screen with Firestore update functionality

--- lib/profile_tab.dart
+++ lib/profile_tab.dart
@@ -1,3 +1,4 @@
+//profile tab
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:shared_preferences/shared_preferences.dart'; // <-- import SharedPreferences
